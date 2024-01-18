50a6525a2fc9e7bff6ef8c9bbd0dd960fb88fcc9
rdas-nev <restrela20@gmail.com>
2024-01-18 17:19:32 +0000

    norminette fix

--- module-1/ex03/load_image.py
+++ module-1/ex03/load_image.py
@@ -10,16 +10,16 @@ def ft_load(path: str) -> np.array:
     try:
         valid_extensions = {'.jpg', '.jpeg'}
         _, file_extension = os.path.splitext(path.lower())
-        
+
         if file_extension not in valid_extensions:
             raise ValueError("Invalid extension. Supported: .jpg .jpeg")
 
         image = Image.open(path)
         image_array = np.array(image)
 
-        print(f"The shape of image is: {image_array.shape}")    
+        print(f"The shape of image is: {image_array.shape}")
         return image_array
-    
+
     except FileNotFoundError as e:
         print(f"Error: {e}")
     except Exception as e:
--- module-1/ex03/zoom.py
+++ module-1/ex03/zoom.py
@@ -9,16 +9,16 @@ def ft_zoom(image_array: np.array):
     Display the zoomed image.
     """
     print(image_array)
-    
+
     zoom_image_array = image_array[100:500, 460:860, :]
 
     grayscale_image = (np.sum(zoom_image_array, axis=2) / 765) * 255
 
-    single_channel_array = grayscale_image[:, :, np.newaxis]
+    one_ch_array = grayscale_image[:, :, np.newaxis]
 
     image_size = Image.fromarray(zoom_image_array).size
-    print(f"New shape after slicing: {single_channel_array.shape} or {image_size}")
-    print(single_channel_array.astype(int))
+    print(f"New shape after slicing: {one_ch_array.shape} or {image_size}")
+    print(one_ch_array.astype(int))
 
-    plt.imshow(single_channel_array, cmap="gray")
+    plt.imshow(one_ch_array, cmap="gray")
     plt.show()
