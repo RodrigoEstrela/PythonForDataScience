d2397b79734612f2968bf912f18eb11ab6851913
rdas-nev <restrela20@gmail.com>
2024-01-18 13:22:56 +0000

    mod1ex02 - fix

--- module-1/ex02/load_image.py
+++ module-1/ex02/load_image.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from PIL import Image
 
@@ -6,8 +7,20 @@ def ft_load(path: str) -> np.array:
     """
     Function to print an image's pixel's rgb content
     """
-    image = Image.open(path)
-    image_array = np.array(image)
+    try:
+        valid_extensions = {'.jpg', '.jpeg'}
+        _, file_extension = os.path.splitext(path.lower())
+        
+        if file_extension not in valid_extensions:
+            raise ValueError("Invalid extension. Supported: .jpg .jpeg")
 
-    print(f"The shape of image is: {image_array.shape}")    
-    return image_array
+        image = Image.open(path)
+        image_array = np.array(image)
+
+        print(f"The shape of image is: {image_array.shape}")    
+        return image_array
+    
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
+    except Exception as e:
+        print(f"Error: {e}")
